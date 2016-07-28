24418507fa1f69f316caf66a858ec0024853b8e8
AlexeyKozlov <kozlov.a@husky.neu.edu>
2016-07-28 10:59:01 -0700

    Rabotaet

---
+++ fixture/contact.py
@@ -0,0 +1,45 @@
+
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+
+    def fillout(self, kon):
+        # create contact
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(kon.First_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(kon.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(kon.nick)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(kon.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(kon.organization)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(kon.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(kon.cell)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(kon.email)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[32]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[32]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(kon.birth)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -15,12 +15,12 @@ def app(request):
 
 def test_kontact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_contact(Contact(First_name="Alexey", last_name="Kozlov", nick="lolo", title="PM", organization="BHCC",address="250 Boston", cell="8574173906",email="alexey.kozlov@bhcc.mass.edu", birth="1988"))
+    app.contact.fillout(Contact(First_name="Alexey", last_name="Kozlov", nick="lolo", title="PM", organization="BHCC", address="250 Boston", cell="8574173906", email="alexey.kozlov@bhcc.mass.edu", birth="1988"))
     app.session.logout()
 
 def test_2_kontact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_contact(Contact(First_name="", last_name="", nick="", title="", organization="", address="", cell="",email="", birth=""))
+    app.contact.fillout(Contact(First_name="", last_name="", nick="", title="", organization="", address="", cell="", email="", birth=""))
     app.session.logout()
 
 
